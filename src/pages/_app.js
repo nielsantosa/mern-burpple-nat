bcf1fb15f5aa17802837f99ac107821724441c27
nielsantosa <nathaniel.santosa@gmail.com>
2024-08-04 12:30:27 +0800

    main: configure tailwind css and set global.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import Layout from '../components/layout'
+import '../styles/global.css'
  
-export default function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   )
 }
+
+export default MyApp
